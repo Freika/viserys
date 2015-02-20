3dd6bac3522925b3bf6a66a445765073e0b353c5
Freika <iamfrey@gmail.com>
2015-02-21 02:06:57 +0600

    Add helper and feature specs for graph helper.

--- spec/factories/post.rb
+++ spec/factories/post.rb
@@ -3,5 +3,6 @@ FactoryGirl.define do
     content 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.'
     status  'daily'
     user_id { create(:user) }
+    created_at Time.now
   end
 end
