94a023ff31e98d6006d913916c070fc0c2a2d6d1
Freika <iamfrey@gmail.com>
2015-02-23 03:51:54 +0600

    Try to make graph spec universal.

--- spec/feature/graph_spec.rb
+++ spec/feature/graph_spec.rb
@@ -3,6 +3,10 @@ require 'spec_helper'
 describe 'Graph' do
   let(:user) { create(:user) }
 
+  before do
+    Timecop.travel(Date.today.year, Date.today.month, Date.today.day, 13, 0, 0)
+  end
+
   it 'shows dark squares on posts#index page' do
     sign_in_with(user.email, user.password)
     create(:post, user_id: user.id)
