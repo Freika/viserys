d2e344453197ef284dfbb7556f577448f8634ad8
Freika <iamfrey@gmail.com>
2015-02-24 21:05:43 +0600

    Add status validation and flash messages.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,10 +8,16 @@ class PostsController < ApplicationController
     elsif params[:year] && params[:week]
       @posts = current_user.posts.week(params[:year], params[:week])
     else
-      @posts = current_user.posts.order(created_at: :desc).first(7)
+      @posts = current_user.posts.order(created_at: :desc).limit(7)
     end
 
-    @posts = @posts.send(params[:status]) if params[:status]
+    if params[:status]
+      if valid_status? params[:status]
+        @posts = @posts.send(params[:status])
+      else
+        redirect_to posts_path, notice: 'Некорректный статус'
+      end
+    end
   end
 
   def show
@@ -61,15 +67,20 @@ class PostsController < ApplicationController
   end
 
   private
-    def set_post
-      @post = Post.find(params[:id])
-    end
 
-    def post_params
-      params.require(:post).permit(:content, :status, :user_id, :created_at)
-    end
+  def valid_status?(status)
+    ['daily', 'weekly', 'monthly', 'yearly'].include?(status)
+  end
 
-    def set_year
-      @year = params[:year].to_i
-    end
+  def set_post
+    @post = Post.find(params[:id])
+  end
+
+  def post_params
+    params.require(:post).permit(:content, :status, :user_id, :created_at)
+  end
+
+  def set_year
+    @year = params[:year].to_i
+  end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -55,4 +55,13 @@ module ApplicationHelper
     end
   end
 
+  def alert_class_for(flash_type)
+    {
+      :success => 'alert-success',
+      :error => 'alert-danger',
+      :alert => 'alert-warning',
+      :notice => 'alert-info'
+    }[flash_type.to_sym] || flash_type.to_s
+  end
+
 end
