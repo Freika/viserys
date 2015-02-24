104d0481f5c1ef6cf670d2732010914475f3b6e7
Freika <iamfrey@gmail.com>
2015-02-24 22:45:06 +0600

    Add username to users and make tests pass.

--- spec/support/users_helpers.rb
+++ spec/support/users_helpers.rb
@@ -2,7 +2,7 @@ module UsersHelpers
   def sign_in_with(email, password)
     visit new_user_session_path
 
-    fill_in 'Email', with: user.email
+    fill_in 'Login', with: user.email
     fill_in 'Password', with: user.password
 
     click_on 'Log in'
