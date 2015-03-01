7036f67248ebcc12d6a68eb8bdf4c8be500dd32e
Freika <iamfrey@gmail.com>
2015-03-01 21:40:55 +0600

    Remove graph partial.

--- spec/feature/graph_spec.rb
+++ spec/feature/graph_spec.rb
@@ -7,7 +7,7 @@ describe 'Graph' do
     Timecop.travel(Date.today.year, Date.today.month, Date.today.day, 13, 0, 0)
   end
 
-  it 'shows dark squares on posts#index page' do
+  pending 'shows dark squares on posts#index page' do
     sign_in_with(user.email, user.password)
     create(:post, user_id: user.id)
 
@@ -16,7 +16,7 @@ describe 'Graph' do
     expect(page).to have_selector('.graph-cell-1')
   end
 
-  it 'shows dark squares on posts#show page' do
+  pending 'shows dark squares on posts#show page' do
     sign_in_with(user.email, user.password)
     post = create(:post, user_id: user.id)
 
