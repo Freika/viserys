7f0632ed6e60778d23e60fa0b8f6fb0c9261854d
Freika <iamfrey@gmail.com>
2015-03-04 20:05:33 +0600

    Add mail settings.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,4 +76,14 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+  address: 'smtp.mandrillapp.com',
+  port: 587,
+  domain: 'days.frey.su',
+  user_name: 'frey@list.ru',
+  password: 'lf_VXIJZFKQpjSIJjldLwg',
+  authentication: 'plain',
+  enable_starttls_auto: true }
 end
